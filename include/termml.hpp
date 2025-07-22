19c3b810abe84a97a9426926d3b4471e212dc43d
Amit Singh <amitsingh19975@gmail.com>
2025-07-23 01:06:38 +0530

    add new header files

--- include/termml.hpp
+++ include/termml.hpp
@@ -1,2 +1,7 @@
 #include "termml/core/raw_mode.hpp"
 #include "termml/core/event.hpp"
+#include "termml/xml/lexer.hpp"
+#include "termml/xml/node.hpp"
+#include "termml/xml/parser.hpp"
+#include "termml/css/style.hpp"
+#include "termml/layout/layout.hpp"
